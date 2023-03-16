5eb1c8b7cfdfb759b37a574bfd3454e87781af4f
Lisa99M <124615334+Lisa99M@users.noreply.github.com>
2023-03-15 19:06:00 -0500

    Add files via upload

---
+++ Taller 1 punto 6.py
@@ -0,0 +1,61 @@
+#6. Escriba un programa que solicite al usuario una letra y determine si es una vocal o una consonante.
+
+letra = input("Ingrese una letra: ")
+
+if letra == 'a':
+    print("La letra " + str(letra) + " ingresada es una vocal.")
+elif letra == 'e':
+    print("La letra " + str(letra) + " ingresada es una vocal.")
+elif letra == 'i':
+    print("La letra " + str(letra) + " ingresada es una vocal.")
+elif letra == 'o':
+    print("La letra " + str(letra) + " ingresada es una vocal.")
+elif letra == 'u':
+    print("La letra " + str(letra) + " ingresada es una vocal.")
+elif letra == 'b':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'c':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'd':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'f':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'g':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'h':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'j':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'k':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'l':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'm':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'n':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'ñ':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'p':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'q':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'r':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 's':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 't':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'v':
+   print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'w':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'x':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'y':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+elif letra == 'z':
+    print("La letra " + str(letra) + " ingresada es consonante.")
+
+else:
+    print("La entrada " + str(letra) + " ingresada no es una letra") 
